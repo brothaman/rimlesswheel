67b722092908c0c23561fee366acd63744d73e08
Robert Brothers <rbrothers12@gmail.com>
2018-04-24 17:48:53 -0500

    calculated and seperated the mass matrix and eom

--- matlab/double_pendulum/double_pendulum_eom.m
+++ matlab/double_pendulum/double_pendulum_eom.m
@@ -67,15 +67,22 @@ dldtheta1 = diff(lagrangian, theta_1);
 dldthetadot1 = diff(lagrangian, thetadot_1);
 ddtdldthetadot1 = ddt(dldthetadot1, theta);
 EOM1 = simplify(dldtheta1 - ddtdldthetadot1);
-EOM1 = collect(EOM1, [thetaddot_1, thetaddot_2, sin(theta_1), sin(theta_2)])
-MM = 
-M11 = 
+EOM1 = collect(EOM1, [thetaddot_1, thetaddot_2, sin(theta_1), sin(theta_2)]);
+
+% seperation of variables to produce the form M*xddot + D = 0
+MM = subs(EOM1,[thetadot_2, g],[0,0])
+M11 = subs(MM, [thetaddot_1,thetaddot_2],[1,0])
+M12 = subs(MM, [thetaddot_1,thetaddot_2],[0,1])
+D1 = EOM1 - MM
+
 dldtheta2 = diff(lagrangian, theta_2);
 dldthetadot2 = diff(lagrangian, thetadot_2);
 ddtdldthetadot2 = ddt(dldthetadot2, theta);
 EOM2 = simplify(dldtheta2 - ddtdldthetadot2);
-EOM2 = collect(EOM2, [thetaddot_1, thetaddot_2, sin(theta_1), sin(theta_2)])
+EOM2 = collect(EOM2, [thetaddot_1, thetaddot_2, sin(theta_1), sin(theta_2)]);
 
+% seperation of variables from the second equation
+MM = subs(EOM2,)
 
 %{
 %% set up the state space equation
