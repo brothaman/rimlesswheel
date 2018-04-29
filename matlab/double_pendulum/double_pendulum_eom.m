972720a098cb632a14dd53a6c3107faf7e7a4aad
brothaman <rbrothers12@gmail.com>
2018-04-28 20:42:54 -0500

    adding all the recent updates to convert calculation to differential

--- matlab/double_pendulum/double_pendulum_eom.m
+++ matlab/double_pendulum/double_pendulum_eom.m
@@ -1,3 +1,4 @@
+%{
 %% Robert Brothers BSME
 % this program will generate the limit cycle for a single over simplified rimless wheel
 % using ODE45
@@ -30,6 +31,8 @@ pcm2 = pcm1 + R_theta1*R_theta2*(lcm2*r_cm2);
 % height of each of the pendulums where theta is the absolute angles from the global ordinate
 h1 = dot(j, pcm1);
 h2 = dot(j, pcm2);
+%h1 = -l1*cos(theta_1)
+h2 = -l1*cos(theta_1) - l2*cos(theta_1+theta_2); % testing temporary
 
 % velocities of each of the pendulums as a function of thetadot
 W1 = [
@@ -54,9 +57,19 @@ K1 = m1*dot(v1,v1)/2 + I1 * thetadot_1^2;
 K2 = m2*dot(v2,v2)/2 + I2 * thetadot_2^2;
 K1 = m1*dot(v1,v1)/2 ;
 K2 = m2*dot(v2,v2)/2 ;
+
+% kinetic energy
+KE = simplify(K1+K2)
+KE = 1/2*m1*l1^2*thetadot_1^2 + 1/2*m2*(l1^2*thetadot_1^2 + l2^2*thetadot_2^2 + 2*l1*l2*thetadot_1*thetadot_2*cos(theta_1 - theta_2));
+% change potential energy 
 P1 = m1*g*h1;
 P2 = m2*g*h2;
-lagrangian = simplify((K1+K2) - (P1+P2));
+
+% potential energy
+PE = simplify(P1 + P2)
+
+% lagrangian KE - PE
+lagrangian = KE - PE;
 
 %% form the equations of motion
 theta = [
@@ -65,6 +78,9 @@ theta = [
 	theta_2, thetadot_2
 	thetadot_2, thetaddot_2
 ];
+% -------------------------------------
+% differentiation with respect to theta
+% -------------------------------------
 dldtheta1 = diff(lagrangian, theta_1);
 dldthetadot1 = diff(lagrangian, thetadot_1);
 ddtdldthetadot1 = ddt(dldthetadot1, theta);
@@ -77,6 +93,9 @@ M11 = subs(MM, [thetaddot_1,thetaddot_2],[1,0]);
 M12 = subs(MM, [thetaddot_1,thetaddot_2],[0,1]);
 D1 = EOM1 - MM;
 
+% ---------------------------------------
+% differentiation with respect to theta 2
+% ---------------------------------------
 dldtheta2 = diff(lagrangian, theta_2);
 dldthetadot2 = diff(lagrangian, thetadot_2);
 ddtdldthetadot2 = ddt(dldthetadot2, theta);
@@ -89,12 +108,15 @@ M21 = subs(MM, [thetaddot_1,thetaddot_2],[1,0]);
 M22 = subs(MM, [thetaddot_1,thetaddot_2],[0,1]);
 D2 = EOM2 - MM
 
+%}
+lagrangian
+eom
 %% set up the state space equation
 % theta1 = x1, theta2 = x2, thetadot1 = x3, thetadot2 = x4
 syms x1 x2 x3 x4 
 M1 = 1;
-M2 = 1;
-G = 9.81; % in the +x direction
+M2 = 2;
+G = -9.81; % in the +x direction
 L1 = 1;
 L2 = 1;
 symbolic_variables = [m1,m2,l1,l2,g,theta_1,thetadot_1,theta_2,thetadot_2];
@@ -113,16 +135,17 @@ D1 = str2func(strcat('@(x1,x2,x3,x4)', char(D1)));
 D2 = str2func(strcat('@(x1,x2,x3,x4)', char(D2)));
 M = @(x) [M11(x(1),x(2),x(3),x(4)), M12(x(1),x(2),x(3),x(4)); M21(x(1),x(2),x(3),x(4)), M22(x(1),x(2),x(3),x(4))];
 D = @(x) [D1(x(1),x(2),x(3),x(4));D2(x(1),x(2),x(3),x(4))];
-x = 1:4;
-xdot(1,x,M,D)
-%xdot_ = @(t,x) M(x(1),x(2),x(3),x(4))\D(x(1),x(2),x(3),x(4))
+
+
 %% perform the ODE45
 options = odeset('RelTol',1e-12,'AbsTol',1e-18);
 t0 = 0;
 tf = 5;
-x0 = [0 0 45*pi/180 0];
-tspan = [t0 tf];
+x0 = [45*pi/180 0 45*pi/180 0];
+tspan = t0:1e-3:tf;
 [T,Y] = ode45(@(t,x)xdot(t,x,M,D), tspan, x0, options);
+
+
 %% plots
 % plot of state
 fig1 = figure;
@@ -145,15 +168,18 @@ hold off
 leg = legend('show');
 set(leg,'Interpreter','latex')
 
+%% animate the pendulum
 % get the endpoints of each of the pendulums
-x1s = L1.*sin(Y(:,1));
-x2s = L2.*sin(Y(:,1)+Y(:,3));
-y1s = -L1.*cos(Y(:,1));
-y2s = -L2.*cos(Y(:,1)+Y(:,3));
+% get equal timesteps
+tstep = t0:tf:(tf-t0)/1e3;
+Y1 = Y(:,1);
+Y2 = Y(:,3);
+x1s = L1.*sin(Y1);
+x2s = x1s+L2.*sin(Y1+Y2);
+y1s = -L1.*cos(Y1);
+y2s = y1s-L2.*cos(Y1+Y2);
 points = [x1s(:),y1s(:),x2s(:),y2s(:)];
 animate_pendulum(points,0.001,[-2,2,-2,1])
-%{
-%}
 
 function [x] = xdot(t,x,M,D)
 x_ = M(x)\D(x);
