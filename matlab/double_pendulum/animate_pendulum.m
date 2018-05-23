65ba28acf8ffce2a88b9570bde84a4751db29a35
brothaman <rbrothers12@gmail.com>
2018-05-23 15:35:38 -0500

    beginning testing of animate

--- matlab/double_pendulum/animate_pendulum.m
+++ matlab/double_pendulum/animate_pendulum.m
@@ -1,19 +1,36 @@
-function fig = animated_pendulum(points,pz,dims)
-fig = figure;
+function animated_pendulum(points, varagin)
+defaults.lims = [2,-2,2,-2];
+defaults.pause = 0;
+defaults.fig = figure;
+defaults.timestep = 1e-3
+
+%% input verification functions
+isValidPointArray = @(x) isnumeric(x) && ~mod(size(x,2),2);
+isGraphLimit = @(x) isnumeric(x) && length(x) == 4 && any(size(x) == 1); % ensure that the input array has 4 numeric values 
+isBool = @(x) x == 1 || x == 0;
+
+p = inputParser;
+addRequired(p,'points',isValidPointArray);
+addOptional(p, 'GraphDimensions',defaults.lims,isGraphLimit)
+addOptional(p, 'Pause', defaults.pause, isBool)
+addOptional(p, 'TimeStep', defaults.timestep, isnumeric)
+parse(p,points,varagin{:});
+
+%% gather the figure
 sz = size(points);
 n = sz(2)/2;
 len = sz(1);
 
 % get axis and plot window
-axis(gca,'equal')
-axis(dims)
+axis(gca,'equal');
+axis(defaults.lims);
 
 % throw for matrices that do not contain pairs of coordinates
-if n ~= int16(n)
-	exception1 = MException('MyComponent:UnevenInput',...
-	'The input matrix of size [%i x %i]''s last dimesion is not even', sz(1),sz(2));
-	throw(exception1)
-end
+%if n ~= int16(n)
+%	exception1 = MException('MyComponent:UnevenInput',...
+%	'The input matrix of size [%i x %i]''s last dimesion is not even', sz(1),sz(2));
+%	throw(exception1)
+%end
 
 for i = 1:len
 	p = points(i,:);
@@ -31,7 +48,7 @@ for i = 1:len
 		P = [origin(:), p_(:)];
 		PP(j) = line(P(1,:),P(2,:));
 	end
-	pause(pz)
+	pause(defaults.timestep)
 	% delete the last points
 	if(i<len)
 		for j = 1:n
