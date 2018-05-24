dcb98c5901609e901504e903512a545a5a9ebb31
brothaman <rbrothers12@gmail.com>
2018-05-24 14:14:18 -0500

    added keyword inputs and input parser

--- matlab/double_pendulum/animate_pendulum.m
+++ matlab/double_pendulum/animate_pendulum.m
@@ -1,38 +1,41 @@
-function animated_pendulum(points, varagin)
+function animated_pendulum(points, varargin)
 defaults.lims = [2,-2,2,-2];
 defaults.pause = 0;
 defaults.fig = figure;
-defaults.timestep = 1e-3
+defaults.timestep = 1e-3;
 
 %% input verification functions
 isValidPointArray = @(x) isnumeric(x) && ~mod(size(x,2),2);
-isGraphLimit = @(x) isnumeric(x) && length(x) == 4 && any(size(x) == 1); % ensure that the input array has 4 numeric values 
-isBool = @(x) x == 1 || x == 0;
+% ensure that the input array has 4 numeric values 
+isGraphLimit = @(x) isnumeric(x) && length(x) == 4 && any(size(x) == 1); 
+isValidTimeStep = @(x) isnumeric(x);
 
-p = inputParser;
-addRequired(p,'points',isValidPointArray);
-addOptional(p, 'GraphDimensions',defaults.lims,isGraphLimit)
-addOptional(p, 'Pause', defaults.pause, isBool)
-addOptional(p, 'TimeStep', defaults.timestep, isnumeric)
-parse(p,points,varagin{:});
+%% parse the input
+parser = inputParser;
+addRequired(parser,'points',isValidPointArray);
+addParameter(parser, 'GraphDimensions',defaults.lims,isGraphLimit)
+addParameter(parser, 'Pause', defaults.pause, @(x) (x==1||x==0))
+addParameter(parser, 'TimeStep', defaults.timestep, isValidTimeStep)
+addParameter(parser, 'Figure', defaults.fig)
+parse(parser,points,varargin{:});
 
 %% gather the figure
-sz = size(points);
+sz = size(points)
 n = sz(2)/2;
 len = sz(1);
 
 % get axis and plot window
 axis(gca,'equal');
-axis(defaults.lims);
+axis(parser.Results.GraphDimensions);
 
-% throw for matrices that do not contain pairs of coordinates
-%if n ~= int16(n)
-%	exception1 = MException('MyComponent:UnevenInput',...
-%	'The input matrix of size [%i x %i]''s last dimesion is not even', sz(1),sz(2));
-%	throw(exception1)
-%end
+% check if user has paused to start
+if parser.Results.Pause
+	pause;
+end
 
+% animate a line between all points given
 for i = 1:len
+	i
 	p = points(i,:);
 	% opportunity for optimization here
 	p_ = [];
@@ -48,7 +51,7 @@ for i = 1:len
 		P = [origin(:), p_(:)];
 		PP(j) = line(P(1,:),P(2,:));
 	end
-	pause(defaults.timestep)
+	pause(parser.Results.TimeStep);
 	% delete the last points
 	if(i<len)
 		for j = 1:n
