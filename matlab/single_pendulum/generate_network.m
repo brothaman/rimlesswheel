e6e97df4b31746954447ecf6ed4654ed69a7c6fd
Robert Brothers <rbrothers12@gmail.com>
2018-06-11 19:44:01 -0500

    adding all edits. swapping machines

--- matlab/single_pendulum/generate_network.m
+++ matlab/single_pendulum/generate_network.m
@@ -13,15 +13,24 @@ torquerange = [-10,10];
 
 all_angles = [0:diff(anglerange)/100:2*pi];
 all_speeds = [-6:12/200:6];
-initialize_NODES(length(all_angles), length(all_speeds));
+all_torques= [-10:1:10];
 
-for torque = -10:1:10
-    % generate new state
-    for x1 = all_angles
-        for x2 = all_speeds
-            [Xin] = generate_new_state( Xin, Xd, torque, time,...
+initialize_NODES(length(all_angles), length(all_speeds));
+for x1 = all_angles
+    for x2 = all_speeds
+        for torque = all_torques
+            generate_new_state( [x1, x2], Xd, torque, time,...
                 anglerange, speedrange, torquerange);
         end
     end
 end
-save('lib/cost_network.m', NODES);
+% for torque = -10:1:10
+%     % generate new state
+%     for x1 = all_angles
+%         for x2 = all_speeds
+%             generate_new_state( Xin, Xd, torque, time,...
+%                 anglerange, speedrange, torquerange);
+%         end
+%     end
+% end
+save('lib/cost_network.m', 'NODES');
