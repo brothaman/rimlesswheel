64591d436521bc511a7a066bd8b5e879f56f0664
brothaman <rbrothers12@gmail.com>
2018-07-19 22:35:22 -0500

    adding the latest parallel:

---
+++ matlab/single_pendulum/dynamic-programming/untitled9.m
@@ -0,0 +1,95 @@
+%% generate all possible states and state transitions
+clear
+addpath ../lib
+xd  = [pi 0];
+torque = 1;
+time = 0.05;
+
+anglerange = [0,2*pi];
+speedrange = [-6,6];
+torquerange = [-10,10];
+
+all_angles = [0:diff(anglerange)/100:2*pi];
+all_speeds = [-6:12/200:6];
+all_torques= [-10:1:10];
+
+nodes = initialize_nodes(all_angles, all_speeds);
+new_nodes = cell(length(all_angles), length(all_speeds));
+parfor i = 1:length(all_angles)
+    node = cell(1,length(all_speeds));
+    for j = 1:length(all_speeds)
+        node{j} = nodes{i,j};
+        for torque = all_torques
+            node{j} = generate_new_state(node{j},xd,torque,time,all_angles,all_speeds);
+        end
+    end
+    new_nodes(i,:) = node;
+end
+% save('lib/cost_network_v0.1.mat', 'NODES');
+
+%% functions
+function nodes = initialize_nodes(angles,speeds)
+    n = length(angles);
+    m = length(speeds);
+    nodes = cell(n,m);
+    nodes(:) = {nNode};
+    for i = 1:n
+        for j = 1:m
+            nodes{i,j}.ID = [i,j];
+            nodes{i,j}.state = [angles(i) speeds(j)];
+        end
+    end
+end
+
+function node = generate_new_state(node,xd,torque,time,angles,speeds)
+    x = [angles(node.ID(1)) speeds(node.ID(2))];
+    upper_angle = max(angles);
+    upper_speed = max(speeds);
+    lower_angle = min(angles);
+    lower_speed = min(speeds);
+    [~,xx] = xnplusone(x,torque,time);
+    % make sure its not the same
+    if xx == x
+        return
+    end
+    
+    % make sure the value is with in range
+    if ( xx(1)>upper_angle || xx(2) > upper_speed || ...
+            xx(1)<lower_angle || xx(2) < lower_speed)
+        return
+    end
+    [xx(1), n] = nearest2(xx(1), angles);
+    [xx(2), m] = nearest2(xx(2), speeds);
+    J = getcost(xx, xd, torque);
+    if isempty(node.connections)
+        node.connections{1} = [n m torque J];
+        return
+    end
+    for i = 1:22
+        if i > length(node.connections)
+            node.connections{i} = [n m torque J];
+            return
+        end
+        if node.connections{i}(1:2) == xx
+            if node.connections{i}(4) > J
+                node.connections{i} = [n m torque J];
+            end
+            return
+        else
+        end
+    end
+end
+
+function J = getcost(x, xd, torque)
+    kx = 1;
+    ka = 1;
+    state_error = dot(x-xd, x-xd) * kx;
+    input_error = torque^2 * ka;
+    J = state_error + input_error;
+end
+
+function [val, n] = nearest2(val,arr)                                                                                                                                                                       
+vec = abs(arr - val);                                                                                                                                                                                       
+[val,n] = min(vec);                                                                                                                                                                                         
+val = arr(val == vec);                                                                                                                                                                                      
+end  
