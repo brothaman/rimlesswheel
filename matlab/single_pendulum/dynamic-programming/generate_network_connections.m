db7d0dbce01972bee7b4c08a7427c20337999815
brothaman <rbrothers12@gmail.com>
2018-07-21 21:16:42 -0500

    updated the network connections function so that it doesnt re-evaluate connections

--- matlab/single_pendulum/dynamic-programming/generate_network_connections.m
+++ matlab/single_pendulum/dynamic-programming/generate_network_connections.m
@@ -14,22 +14,23 @@ else
 end
 network = convert_network(network);
 clearvars -except network p
-steps = 3;
+steps = 5;
 ids = network(~any([51 101] - network(:,[1 2]),2),[1 2 4 5]);
 connections = cell(steps,1);
 for i = 1:steps
     tic
     if i > 1
         connections{i} = parnetwork_search3(network, ids, previous_ids);
+        previous_ids = [previous_ids; ids];
     else
         connections{i} = network_search3(network, ids(1,1:2));
+        previous_ids = ids;
     end
     t = seconds(toc);
     t.Format = 'hh:mm:ss.SSS';
     t
     i
-    save('cost_network_v1.0.mat','i','network','connections', 't')
-    previous_ids = ids;
+%     save('cost_network_v1.0.mat','i','network','connections', 't')
     ids = cell2mat(connections{i});
     ids = ids(:,[1 2 4 5]);
 end
@@ -43,10 +44,10 @@ function [connections] = parnetwork_search3(network, ids, previous_ids)
         % id's [4,5] is equal to previous id's [1,2] then eliminate current
         % id's connection that correlate with [4,5]. delete current id's
         % [4,5]
-        connections{i} = network(~any(ids(i,1:2) - network(:,[4,5]),2),:);
+        connections{i} = network(~any(ids(i,[1,2]) - network(:,[4,5]),2),:);
         % this will eliminate any connections back to the previous
         for j = 1:size(previous_ids,1)
-            connections(i) = {connections{i}(any(previous_ids(j,[1 2 4 5]) - connections{i}(:,[1 2 4 5]),2),:)};
+            connections(i) = {connections{i}(any(previous_ids(j,:) - connections{i}(:,[1 2 4 5]),2),:)};
         end
     end
 end
@@ -55,7 +56,7 @@ function [connections] = network_search3(network, ids)
     len = size(ids,1);
     connections = cell(len,1);
     for i = 1:len
-        connections{i} = network(~any(ids(i,:) - network(:,[4,5]),2),:);
+        connections{i} = network(~any(ids(i,[1,2]) - network(:,[4,5]),2),:);
     end
 end
 
