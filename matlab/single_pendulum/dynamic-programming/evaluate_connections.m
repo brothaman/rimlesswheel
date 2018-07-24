a920b0e43e4e8144511a8ea17863cf985db71c2d
Robert Brothers <rbrothers12@gmail.com>
2018-07-24 16:34:50 -0500

    adding some updates to store information after each iteration and print out the iteration time

--- matlab/single_pendulum/dynamic-programming/evaluate_connections.m
+++ matlab/single_pendulum/dynamic-programming/evaluate_connections.m
@@ -19,6 +19,7 @@ net = cell(size(network));
 n = size(connections,1);
 for i = 1:n
     m = size(connections{i},1);
+    tic
     for j = 1:m
         [l,p] = size(network);
         for k = 1:size(connections{i}{j},1)
@@ -27,33 +28,35 @@ for i = 1:n
             for l = 1:N
                 for p = 1:M
                     if connection(1:2) == [l p]
-                        network{l,p} = evaluate_connection(network,network{l,p}, network{l,p}.connections{connection(3)});
+                        network{l,p} = evaluate_connection(network,network{l,p}, [network{l,p}.connections{connection(3)} connection(3)]);
                     end
                 end
             end
         end
-        j
     end
+    t = seconds(toc);
+    t.Format = 'HH:mm:ss.SSS'
     i
 end
+save('../lib/cost_network_v2.0.mat','network');
 %% functions
 function node = evaluate_connection(network,node, connection)
-    if isempty(network{connection(4), connection(5)}.optimal_value)
+    if isempty(network{connection(1), connection(2)}.optimal_value)
         return
     else
         % since the destination node has been evaluated we can now
         % determine whether the connection is desireable
         % if the policy and value do not exit
         % if  isempty(node.optimal_value) && isempty(node.optimal_policy)
-        value = connection(7) + network{connection(4), connection(5)}.optimal_value;
+        value = connection(4) + network{connection(1), connection(2)}.optimal_value;
         if  isempty(node.optimal_value)
-            node.optimal_policy = connection(3);
+            node.optimal_policy = connection(5);
             node.optimal_value = value;
         else
             % compare the node and update if a better policy exist
             % otherwise do nothing
             if node.optimal_value > value
-                node.optimal_policy = connection(3);
+                node.optimal_policy = connection(5);
                 node.optimal_value = value;
             end
         end
