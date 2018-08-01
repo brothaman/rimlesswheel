7ed1ab93f6ec9b4d4a7f82eccc5e839c75eb21e0
brothaman <rbrothers12@gmail.com>
2018-08-01 09:26:26 -0500

    adding a standard initialization profile to the evaluation connections script

--- matlab/single_pendulum/dynamic-programming/evaluate_connections.m
+++ matlab/single_pendulum/dynamic-programming/evaluate_connections.m
@@ -10,7 +10,7 @@ clear
 % network{51,101}.optimal_value = 0;
 % network{51,101}.optimal_policy = 9;
 
-[filename,i,network,connection_network,connections, t, ids,previous_ids] = very_weak_actuation_init();
+[filename,i,network,connection_network,connections, t, ids,previous_ids] = standard_init();
 % for each level of connection in the connection network cycle through all
 % the nodes and evaluate the connection. if a connection exist and the
 % compare the value and store the policy and value of the lower value
@@ -67,6 +67,14 @@ function node = evaluate_connection(network,node, connection)
             end
         end
     end
+end
+
+function [filename,i,network,connection_network,connections, t, ids,previous_ids] = standard_init()
+    filename = '../lib/cost_network.mat';
+    load(filename);
+    % set the goal node 
+    network{51,101}.optimal_value = 0;
+    network{51,101}.optimal_policy = 9;
 end
 
 function [filename,i,network,connection_network,connections, t, ids,previous_ids] = underactuated_init()
